7c79d3b234cba21fe26377c6b3e577d2e2fab69d
Luis González <lluisgon012@gmail.com>
2024-05-31 20:27:52 +0200

    Revert "Wwise project done"
    This reverts commit db0dd42401f5e078cc8a92f0959eb1dd6acac6aa.

--- 3_wwise_project/Wwise Adventure Game 2022.1.0.230/WwiseAdventureGame/WwiseAdventureGame_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ 3_wwise_project/Wwise Adventure Game 2022.1.0.230/WwiseAdventureGame/WwiseAdventureGame_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -401,16 +401,6 @@ namespace AK
         static const AkUniqueID PLAYER_HEALTH = 215992295U;
         static const AkUniqueID PLAYER_MOVEMENTSPEED = 2471758783U;
         static const AkUniqueID QUEST_PROGRESS = 500836401U;
-        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
-        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
-        static const AkUniqueID SS_AIR_FURY = 1029930033U;
-        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
-        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
-        static const AkUniqueID SS_AIR_RPM = 822163944U;
-        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
-        static const AkUniqueID SS_AIR_STORM = 3715662592U;
-        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
-        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
         static const AkUniqueID TIME_OF_DAY = 207908541U;
     } // namespace GAME_PARAMETERS
 
